9af4a05c9079a875ebf1084cabdec33a445dde8d
lautaromendivil <lautaromendivil8@gmail.com>
2026-09-13 17:29:16 -0300

    feat: tipar props de AutorizacionesProvider con PropTypes

--- src/context/AutorizacionesContext.jsx
+++ src/context/AutorizacionesContext.jsx
@@ -1,37 +1,35 @@
 import { createContext, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 export const AutorizacionesContext = createContext()
 
 const AutorizacionesProvider = ({ children }) => {
-
-  const [admin, setAdmin] = useState(()=>{
-    const adminGuardado= localStorage.getItem('admin')
-    if(adminGuardado){
+  const [admin, setAdmin] = useState(() => {
+    const adminGuardado = localStorage.getItem('admin')
+    if (adminGuardado) {
       return JSON.parse(adminGuardado)
     }
     return null
   })
-useEffect(()=>{
-  if(admin){
-    localStorage.setItem(
-      'admin',
-      JSON.stringify(admin)
-    )
-  }else{
-    localStorage.removeItem('admin')
+  useEffect(() => {
+    if (admin) {
+      localStorage.setItem('admin', JSON.stringify(admin))
+    } else {
+      localStorage.removeItem('admin')
+    }
+  }, [admin])
+  const cerrarSesion = () => {
+    setAdmin(null)
   }
-
-},[admin])
-const cerrarSesion=()=>{
-  setAdmin(null)
-}
-return (
-    <AutorizacionesContext.Provider
-      value={{ admin, setAdmin, cerrarSesion }}
-    >
+  return (
+    <AutorizacionesContext.Provider value={{ admin, setAdmin, cerrarSesion }}>
       {children}
     </AutorizacionesContext.Provider>
   )
+}
+
+AutorizacionesProvider.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
 export default AutorizacionesProvider
